f54b7282482970f83e70c7bed817a0cae22182c9
Arturo Rosales V <rosalesvelazquezarturo@gmail.com>
2025-09-27 14:44:10 -0600

    Debugging backend uri redirect

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,7 +9,7 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI || (
   process.env.NODE_ENV === 'production' 
-    ? `${process.env.FRONTEND_URL}/login`
+    ? 'https://app.mydoctrack.com/login'
     : 'http://localhost:5173/login'
 );
 
